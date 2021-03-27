de240f57b5d858f458690996c0e2776e346d5f27
Krutik Raut <58563133+reboot13-git@users.noreply.github.com>
2021-03-27 11:12:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-getData = function(){
- 
-let url = document.getElementById('url').value;
-
-   fetch(url)
-   .then(response => response.json())
-   .then(data => {
-       document.getElementById('jsonData').innerHTML = JSON.stringify(data);
-   })
-
-}
+function getData(){
+    var url = document.getElementById('url').value;
+    var myHeaders = new Headers();
+    myHeaders.append('Access-Control-Allow-Origin', 'https://github.com');
+       fetch(url,{
+           method: 'GET',
+           headers: myHeaders
+       })
+       .then(response => response.json())
+       .then(data => {
+           document.getElementById('jsonData').innerHTML = JSON.stringify(data);
+       })
+    
+    }
