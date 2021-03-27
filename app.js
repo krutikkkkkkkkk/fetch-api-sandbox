a74a2a10e7fbb1d2f7f0f516ea183d643338cc61
Krutik Raut <58563133+reboot13-git@users.noreply.github.com>
2021-03-27 11:21:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 function getData(){
     var url = document.getElementById('url').value;
-    var myHeaders = new Headers();
-    myHeaders.append('Access-Control-Allow-Origin', 'https://reboot13-git.github.io/');
+    
        fetch(url,{
            method: 'GET',
-           headers: myHeaders
+           mode: 'no-cors'
        })
        .then(response => response.json())
        .then(data => {
            document.getElementById('jsonData').innerHTML = JSON.stringify(data);
+       })
+       .catch(error => {
+    document.getElementById('error').innerHTML = error);
        })
     
     }
